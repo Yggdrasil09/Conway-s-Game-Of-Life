0818eabcc667450444ab1f3f779872c8629a39cb
Yggdrasil09 <karthik99th.tnk@gamil.com>
2019-01-04 20:53:49 +0530

    Created the grid and the first generation is set

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-
+let grid = new Array(135);
 
 function setup()
 {
-    createCanvas(1350, 600,);
-    background(0);
+    for(let i = 0;i < 135; i++ )
+    {
+        grid[i] = new Array(60);
+    }
+    createCanvas(1350, 600);
+    for(let i = 0;i < 135; i++)
+    {
+        for(let j = 0;j < 60; j++)
+        {
+            grid[i][j] = floor(random(2));
+        }
+    }
 }
 
 function draw()
 {
-
+    for(let i = 0; i < 135 ; i++)
+    {
+        for(let j = 0;j < 60 ; j++)
+        {
+            if(grid[i][j])
+            {
+                fill(0);
+                rect(i*10,j*10,10,10);
+            }
+            else
+            {
+                fill(255);
+                rect(i*10,j*10,10,10);
+            }
+        }
+    }
 }
