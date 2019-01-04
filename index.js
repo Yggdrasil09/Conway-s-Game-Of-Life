76ddc60160b2e59812aa4a983621d6561aefd20c
Yggdrasil09 <karthik99th.tnk@gamil.com>
2019-01-05 00:46:59 +0530

    The simulation and logic of evolution is done

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-let grid = new Array(135);
+let cols = 135 ;
+let rows = 60 ;
+
+let grid = new Array(cols);
 
 function setup()
 {
-    for(let i = 0;i < 135; i++ )
+    for(let i = 0;i < cols; i++ )
     {
-        grid[i] = new Array(60);
+        grid[i] = new Array(rows);
     }
+
     createCanvas(1350, 600);
-    for(let i = 0;i < 135; i++)
+    
+    for(let i = 0;i < cols; i++)
     {
-        for(let j = 0;j < 60; j++)
+        for(let j = 0;j < rows; j++)
         {
             grid[i][j] = floor(random(2));
         }
@@ -18,20 +23,55 @@ function setup()
 
 function draw()
 {
-    for(let i = 0; i < 135 ; i++)
+    background(0);
+    for(let i = 0;i < cols ; i++)
     {
-        for(let j = 0;j < 60 ; j++)
+        for(let j = 0;j < rows ; j++)
         {
-            if(grid[i][j])
-            {
-                fill(0);
-                rect(i*10,j*10,10,10);
-            }
-            else
+            if(grid[i][j] == 1)
             {
                 fill(255);
+                stroke(0);
                 rect(i*10,j*10,10,10);
             }
         }
     }
+    
+    let next = new Array(cols);
+
+    for(let i = 0;i < cols; i++ )
+    {
+        next[i] = new Array(rows);
+    }
+
+    let count = 0; 
+
+    for(let i = 0; i < cols ; i++)
+    {
+        for(let j = 0; j < rows ; j++)
+        {
+            let state = grid[i][j];
+            count = 0;
+            for( let k = -1 ; k < 2; k++)
+            {
+                for(let l = -1 ; l < 2; l++)
+                {
+                    let col = (i + k + cols) % cols;
+                    let row = (j + l + rows) % rows;
+                    count += grid[col][row];
+                }
+            }
+            count -= grid[i][j];
+
+            if (state == 0 && count == 3) {
+                next[i][j] = 1;
+            } else if (state == 1 && ( count < 2 || count > 3)) {
+                next[i][j] = 0;
+            } else {
+                next[i][j] = state;
+            }
+        }
+    }
+
+    grid = next ;
 }
